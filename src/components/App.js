65b1ec4b8a02bcea948048f9fa6986f52a307b7f
Akshaya <akshayaganesh0910@gmail.com>
2025-06-05 22:22:15 -0700

    initial message

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
-
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () =>{
+    setCount(count + 1);
+  };
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {count} {count === 1 ? "time" : "times"}</p>
+        <button onClick={handleIncrement}>Click me</button>
+        
     </div>
   )
 }
